5c91feb679cfddb04af16086ecaf7e8e5816af9e
Nikolay <grudanov.n.a@gmail.com>
2022-03-20 02:16:17 +0300

    исправил дашборд

--- nvim-setting/lua/dashboard.lua
+++ nvim-setting/lua/dashboard.lua
@@ -22,13 +22,13 @@ g.dashboard_session_directory = vim.fn.stdpath('data') .. '/sessions'
  }
 
 g.dashboard_custom_section = {
-  a = { description = {  icons.find .. " Find File " }, command = "Telescope find_files" },
+  a = { description = {  icons.file .. " Find File " }, command = "Telescope find_files" },
   b = { description = {  icons.clock2 .. " Recently used files " }, command = "Telescope oldfiles" },
  -- c = { description = {  icons.file2 .. " Find Word " }, command = "Telescope live_grep" },
   d = { description = {  icons.pluse .. " New File " }, command = "DashboardNewFile" },
   e = { description = {  icons.bookmark .. " Bookmarks " }, command = "Telescope marks" },
  -- f = { description = {  icons.last .. " Last Session " }, command = "SessionLoad" },
-  g = { description = {  icons.file .. " Find project " }, command = "Telescope projects"}, 
+  g = { description = {  icons.file2 .. " Find project " }, command = "Telescope projects"}, 
   h = { description = {  icons.config .. " Configuration " }, command = ":e ~/.config/nvim/init.vim"},
   i = { description = {  icons.error2 .. " Quit Neovim " }, command = ":qa"},
 }
