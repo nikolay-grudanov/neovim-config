99ab225bc11dcaab8a8c0b825a1c98c8010f4259
Груданов Николай <grudanov.n.a@gmail.com>
2023-04-16 05:51:05 +0300

    Атоматическая смена нумерации и другой рефакторинг

--- lua/user/setting.lua
+++ lua/user/setting.lua
@@ -1,28 +1,45 @@
-local cmd = vim.cmd             -- execute Vim commands
-local exec = vim.api.nvim_exec  -- execute Vimscript
-local g = vim.g                 -- global variables
-local opt = vim.opt             -- global/buffer/windows-scoped options
--- Глобальные переменные 
+local cmd = vim.cmd -- execute Vim commands
+local exec = vim.api.nvim_exec -- execute Vimscript
+local g = vim.g -- global variables
+local opt = vim.opt -- global/buffer/windows-scoped options
+-- Глобальные переменные
 ---------------------------------------------------------------------------
 -- g.vimspector_enable_mappings = 'VISUAL_STUDIO'
 ---------------------------------------------------------------------------
 -- Настройки
 ---------------------------------------------------------------------------
-cmd [[:set keymap=russian-jcukenwin]]
+cmd([[:set keymap=russian-jcukenwin]])
 -- Дополнительно можно добавить  Чтобы вместо Ctrl-^ нажимать Ctrl-L
-cmd [[:inoremap <C-l> <C-^>]]
+cmd([[:inoremap <C-l> <C-^>]])
 opt.termguicolors = true --  24-bit RGB colors
 opt.iminsert = 0 -- Чтобы при старте ввод был на английском, а не русском (start > i)
 opt.imsearch = 0 -- Чтобы при старте поиск был на английском, а не русском (start > /)
 -- Подсвечивает на доли секунды скопированную часть текста
-exec([[
+exec(
+	[[
     augroup YankHighlight
     autocmd!
     autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=150}
     augroup end
-]], false)
+]],
+	false
+)
 -- Направление перевода с русского на английский
 -- g.translate_source = 'ru'
 -- g.translate_target = 'en'
 --------------------------------------------------------------------------
+-- Функции
+-- Автоматическая смена нумерации
+vim.api.nvim_create_autocmd("InsertEnter", {
+	callback = function()
+		vim.opt.relativenumber = false
+	end,
+})
 
+vim.api.nvim_create_autocmd("InsertLeave", {
+	callback = function()
+		vim.opt.relativenumber = true
+	end,
+})
+
+--------------------------------------------------------------------------
