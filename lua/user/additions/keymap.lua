4830289a96c95237a3f4ea978696d3c962f202d5
Николай Груданов <grudanov.n.a@gmail.com>
2023-09-21 02:14:04 +0300

    Добавил для линукс и виндовс переходы черех Alt+l Alt+h

--- lua/user/additions/keymap.lua
+++ lua/user/additions/keymap.lua
@@ -7,10 +7,22 @@ vim.keymap.set("n", "[c", function()
     require("todo-comments").jump_prev()
 end, { desc = "Previous comment" })
 
+-- Переключения табов через Alt+l Alt+h 
+
+-- Mac
 vim.keymap.set("n", "¬", function()
     require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1)
 end, { desc = "Next buffer" })
 
 vim.keymap.set("n", "˙", function()
+    require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
+end, { desc = "Previous buffer"})
+
+-- Linux, Windows
+vim.keymap.set("n", "<M-l>", function()
+    require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1)
+end, { desc = "Next buffer" })
+
+vim.keymap.set("n", "<M-h>", function()
     require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
 end, { desc = "Previous buffer"})
